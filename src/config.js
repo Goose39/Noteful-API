86af132c19efeb299409ab94cf3145ac7f6a9d43
Goose39 <Goose39.Dev@gmail.com>
2020-01-17 12:59:08 -0500

    heroku database env variables

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DB_URL: process.env.DB_URL
+  API_TOKEN: process.env.API_TOKEN,
+  DATABASE_URL: process.env.DATABASE_URL
 }
